4cddc7adfe7a25d298d4cc7477133ad3788dbf59
Candace Eckels <candace.eckels@gmail.com>
2022-03-09 17:00:33 -0700

    Add array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,9 @@ arrays = ["Nick", "Candace", "Matt", "Ed", "Tim"]
 puts arrays[2]
 puts arrays[4]
 p arrays[0]
+
+places = ["San Diego", "Ohio", "Denver", "Utah", "Texas"]
+places.each do |place|
+    puts "Your are Located in #{place}"
+  end
+   
